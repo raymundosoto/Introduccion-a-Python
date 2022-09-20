4c4ab4e053a39a92730455bbf06f6282f5f50c84
M. En C. Raymundo Soto <raymundosotosoto@hotmail.com>
2022-09-20 13:54:18 -0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -3,5 +3,10 @@ x = 5
 y = 1 + x * 2 # variable con una operación matemática 
 z = "hola a todos" # variable de caracteres 
 w = input ("ingresa tu color favorito") # variable de ingreso de caracteres 
-cantidad = int (input ("Ingresa tu edad")) # variable de ingreso numérico entero 
+edad= int (input ("Ingresa tu edad")) # variable de ingreso numérico entero 
 peso = float (input ("¿cuánto pesas?")) # variable de ingreso de tipo decimal 
+
+#imprime los datos 
+print("El valor de x es:", x) 
+print("El valor de y es:", y) 
+print("Tu color favorito es ",w, ", tienes", edad, " años y pesas ",peso," kg")
