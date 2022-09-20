4ebb78f18af35a9e39db4c427bf4d62d07373346
M. En C. Raymundo Soto <raymundosotosoto@hotmail.com>
2022-09-20 13:52:11 -0500

    Uso de variables en python
    Uso de variables

---
+++ variables.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-c
+x = 5
+y = 1 + x * 2 # variable con una operación matemática 
+z = "hola a todos" # variable de caracteres 
+w = input ("ingresa tu color favorito") # variable de ingreso de caracteres 
+cantidad = int (input ("Ingresa tu edad")) # variable de ingreso numérico entero 
+peso = float (input ("¿cuánto pesas?")) # variable de ingreso de tipo decimal 
