d5b5a3c6fc0635b60e60ed061ffe560f445eaaf3
M. En C. Raymundo Soto <raymundosotosoto@hotmail.com>
2022-09-22 12:29:07 -0500

    Manejo de funciones en python

--- estructuras-de_control.py
+++ estructuras-de_control.py
@@ -2,16 +2,17 @@
 num = float(input("Ingresa un número: ")) 
 # condición uno 
 if num > 0: 
-    adjective = " " 
+    #adjective = " " 
     if num >= 1000000: 
         adjective = " muy grande " 
     elif num >= 1000: 
         adjective = " grande " 
     # El resultado es una cadena con el adjetivo y el tipo de número 
     result = "Es realmente un número" + adjective + "y positivo" 
+    print(result)
 elif num < 0: 
     result = "Es un número negativo" 
 else: 
     result = "Este número es cero" 
 # Imprime el resultado 
-    print(result)
+print(result)
